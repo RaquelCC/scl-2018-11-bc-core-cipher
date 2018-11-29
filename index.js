bd056456914cfb1d037e0a09cb2572c8b2f5ae9a
RaquelCC <rcanalesconcha@gmail.com>
2018-11-29 11:39:48 -0300

    primera version con todo lo básico funcionando

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-/* Acá va tu código */
+window.onload = () => {
+
+    
+    
+    const rootDiv = document.getElementById("root");
+    
+    const pantallaCifradoDiv = document.getElementById("pantallaCifrado");
+    
+    const botonComenzar = document.getElementById("comenzar");
+    
+    botonComenzar.addEventListener("click", ()=> {
+        rootDiv.style.display = "none";
+        pantallaCifradoDiv.style.display = "inline-block";
+    })
+    
+    const botonCipher = document.getElementById("cipher");
+    
+    botonCipher.addEventListener("click", () => {
+        let texto = document.getElementById("texto").value;
+        let offset = document.getElementById("offset").value;
+        document.getElementById("pantallaResultado").innerHTML = window.cipher.encode(offset, texto);
+        document.getElementById("pantallaResultado").style.display = "inline-block";
+        document.getElementById("regresarInicio").style.display = "inline-block";
+    });
+    
+    const botonDecipher = document.getElementById("decipher");
+    
+    botonDecipher.addEventListener("click", () => {
+        let texto = document.getElementById("texto").value;
+        let offset = document.getElementById("offset").value;
+        document.getElementById("pantallaResultado").innerHTML = window.cipher.decode(offset, texto);
+        document.getElementById("pantallaResultado").style.display = "inline-block";
+        document.getElementById("regresarInicio").style.display = "inline-block";
+    })
+
+    const botonInicio = document.getElementById("inicio");
+    botonInicio.addEventListener("click", () => {
+        document.location = "index.html";
+    })
+
+
+}
