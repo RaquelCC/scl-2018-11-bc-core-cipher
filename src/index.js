2ebbd8c309665ddfb523bc882d2b2e37c81e2cf7
RaquelCC <rcanalesconcha@gmail.com>
2018-11-30 23:04:35 -0300

    probando con materialize y ajustes a los enter en las casillas de cifrado y descifrado

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ window.onload = () => {
     
     botonComenzar.addEventListener("click", ()=> {
         rootDiv.style.display = "none";
-        pantallaCifradoDiv.style.display = "inline-block";
-        document.getElementById("pantallaResultado").style.display = "inline-block";
-        document.getElementById("offsetYOtros").style.display = "inline-block";
+        pantallaCifradoDiv.style.display = "block";
+        document.getElementById("pantallaResultado").style.display = "block";
+        document.getElementById("offsetYOtros").style.display = "block";
 
     })
     
@@ -29,6 +29,23 @@ window.onload = () => {
         document.getElementById("textoADescifrar").value = cipher.encode(offset, texto);
         // document.getElementById("regresarInicio").style.display = "inline-block";
     });
+
+    const textareaCifrar = document.getElementById("textoACifrar");
+
+    textareaCifrar.addEventListener("keydown", (e) => {
+        if (e.keyCode === 13 && textareaCifrar.value && !e.shiftKey) {
+            let texto = document.getElementById("textoACifrar").value;
+            let offset = document.getElementById("offset").value;
+            offset = Number(offset);
+            if (Number.isNaN(offset) || !offset) {
+            textareaCifrar.blur();
+            alert('Debes ingresar un número en el offset.');
+            return;
+            }
+            document.getElementById("textoADescifrar").value = cipher.encode(offset, texto);
+            textareaCifrar.blur();
+        }
+    })
     
     const botonDecipher = document.getElementById("decipher");
     
@@ -43,10 +60,27 @@ window.onload = () => {
         // document.getElementById("regresarInicio").style.display = "inline-block";
     })
 
-    // const botonInicio = document.getElementById("inicio");
-    // botonInicio.addEventListener("click", () => {
-    //     document.location = "index.html";
-    // })
+    const textareaDescifrar = document.getElementById("textoADescifrar");
+
+    textareaDescifrar.addEventListener("keydown", (e) => {
+        if (e.keyCode === 13 && textareaDescifrar.value && !e.shiftKey) {
+            let texto = document.getElementById("textoADescifrar").value;
+            let offset = document.getElementById("offset").value;
+            offset = Number(offset);
+            if (Number.isNaN(offset) || !offset) {
+            textareaDescifrar.blur();
+            alert('Debes ingresar un número en el offset.');
+            return;
+            }
+            document.getElementById("textoACifrar").value = cipher.encode(offset, texto);
+        textareaDescifrar.blur();
+        }
+    });
+
+    const botonInicio = document.getElementById("inicio");
+    botonInicio.addEventListener("click", () => {
+        document.location = "index.html";
+    })
 
 
 }
